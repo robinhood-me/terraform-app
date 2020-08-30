4c97e67edc2a0b03ac7f2e361344e14210979a7f
Robin Hood <rhcrco@gmail.com>
2020-08-30 15:48:41 -0700

    change ami_id to actual rhel ami_id in us-west-2

--- dev/main.tf
+++ dev/main.tf
@@ -13,7 +13,7 @@ module "my_vpc" {
 module "my_ec2" {
   source        = "../modules/ec2"
   ec2_count     = 1
-  ami_id        = "ami-5a8da735"
+  ami_id        = "ami-02f147dfb8be58a"
   instance_type = "t2.micro"
   subnet_id     = "${module.my_vpc.subnet_id}"
 }
